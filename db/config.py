51146f5b40feab0df561cfefdf1c48c4e016a4f9
mbgoodguy <nerooren4001@gmail.com>
2024-03-26 19:04:02 +0300

    Add refactoring through db_session_method decorator

--- db/config.py
+++ db/config.py
@@ -1,4 +1,5 @@
 import os
+from contextlib import contextmanager
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -28,59 +29,66 @@ settings = DB_Settings()
 engine = create_engine(url=settings.DB_URL, echo=True)
 
 
+def db_session_method(method):
+    def wrapper(cls, *args, **kwargs):
+        with cls.get_db_session() as db:
+            return method(cls, db, *args, **kwargs)
+    return wrapper
+
+
 class TodoTools:
+    engine = create_engine(url=settings.DB_URL, echo=True)
     session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-    # CRUD methods
     @classmethod
     def create_tables(cls):
-        Base.metadata.create_all(engine)
+        Base.metadata.create_all(cls.engine)
         print(f'Таблицы созданы')
 
     @classmethod
-    def add_todo(cls, todo_data: TodoPayload):
+    @db_session_method
+    def add_todo(cls, db, todo_data: TodoPayload):
         todo = TodoTable(title=todo_data.title, description=todo_data.description, completed=todo_data.completed)
-
-        with cls.session(autocommit=False, autoflush=False, bind=engine) as db:
-            db.add(todo)
-            db.commit()
-            db.refresh(todo)
-        print(f'TYPE OF todo var: {type(todo)}')
+        db.add(todo)
+        db.commit()
+        db.refresh(todo)
         return todo
 
     @classmethod
-    def get_all_todos(cls):
-        with cls.session(autocommit=False, autoflush=False, bind=engine) as db:
-            res = db.query(TodoTable).all()
-
-            return res
+    @db_session_method
+    def get_all_todos(cls, db):
+        return db.query(TodoTable).all()
 
     @classmethod
-    def delete_todo_by_id(cls, pk: int):
-        with cls.session(autocommit=False, autoflush=False, bind=engine) as db:
-            todo = db.get(TodoTable, pk)
-
-            if todo:
-                db.delete(todo)
-                db.commit()
-                # return todo
-                return f'Todo with id {pk} deleted'
-            else:
-                raise_not_exist(pk)
+    @db_session_method
+    def delete_todo_by_id(cls, db, pk: int):
+        todo = db.get(TodoTable, pk)
+        if todo:
+            db.delete(todo)
+            db.commit()
+            return f'Todo with id {pk} deleted'
+        else:
+            raise_not_exist(pk)
 
     @classmethod
-    def update_todo(cls, pk: int, todo_data: TodoPayload):
-        with cls.session(autocommit=False, autoflush=False, bind=engine) as db:
-            todo = db.get(TodoTable, pk)
-            print(todo)
+    @db_session_method
+    def update_todo(cls, db, pk: int, todo_data: TodoPayload):
+        todo = db.get(TodoTable, pk)
+        if todo:
+            todo.title = todo_data.title
+            todo.description = todo_data.description
+            todo.completed = todo_data.completed
+            db.add(todo)
+            db.commit()
+            return f'Todo with id {pk} updated'
+        else:
+            raise_not_exist(pk)
 
-            if todo:
-                todo.title = todo_data.title
-                todo.description = todo_data.description
-                todo.completed = todo_data.completed
-
-                db.add(todo)
-                db.commit()
-                return f'Todo with id {pk} updated'
-            else:
-                raise_not_exist(pk)
+    @classmethod
+    @contextmanager
+    def get_db_session(cls):
+        db = cls.session()
+        try:
+            yield db
+        finally:
+            db.close()
